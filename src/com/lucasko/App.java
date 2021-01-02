9aec1f8d3afd9be8bf4301b0f3e446f0c6594187
Lucas Ko <lucasko.tw@gmail.com>
2021-01-02 15:30:39 +0800

    init

---
+++ src/com/lucasko/App.java
@@ -0,0 +1,17 @@
+package com.lucasko;
+
+import org.example.MyLibs;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!!");
+		
+		MyLibs mylibs = new MyLibs();
+		
+		mylibs.showInfo();
+	}
+
+}
